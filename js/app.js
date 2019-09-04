a5a399d971179268c4e5de160b99b20c78195af2
kanoe <noe.ngongo20@gmail.com>
2019-09-04 23:03:03 +0200

    color + js

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const userScore = 0;
+const computerScore = 0;
+const userScore_Spam  = document.getElementById("user-score");
+const computerScore_Spam  = document.getElementById("computer-score");
